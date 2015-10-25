2fbf466003d0c5549ce878b9bd5e5ecf03a68b25
DomHeal <Dominic.heal@europe.com>
2015-10-25 12:22:22 +0000

    Partial GUI implementation

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,91 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import javax.mail.*;
+import java.util.Properties;
+
 public class Controller {
+
+    @FXML private PasswordField passwordField;
+    @FXML private TextField emailField;
+
+    public void connectButtonAction() {
+        System.out.println("-----------------------");
+        System.out.println("Attempting to Connect");
+        System.out.println("-----------------------");
+        String host = "pop.gmail.com";
+        String mailStoreType = "pop3";
+        String username = emailField.getText();
+        String password = passwordField.getText();
+
+        System.out.println("Credentials Acquired!");
+        System.out.println("Email: " + username);
+        System.out.println("Password: " + password);
+        System.out.println("-----------------------");
+
+
+
+        check(host, mailStoreType, username, password);
+    }
+
+    public static void check(String host, String storeType, final String user,
+                             final String password)
+    {
+        try {
+
+            // create properties field
+            Properties properties = new Properties();
+
+            properties.put("mail.pop3s.host", host);
+            properties.put("mail.pop3s.port", "995");
+            properties.put("mail.pop3s.starttls.enable", "true");
+
+
+            // Setup authentication, get session
+            Session emailSession = Session.getInstance(properties,
+                    new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(
+                                    user, password);
+                        }
+                    });
+            // emailSession.setDebug(true);
+
+            // create the POP3 store object and connect with the pop server
+            Store store = emailSession.getStore("pop3s");
+
+            store.connect();
+
+            // create the folder object and open it
+            Folder emailFolder = store.getFolder("INBOX");
+            emailFolder.open(Folder.READ_ONLY);
+
+            // retrieve the messages from the folder in an array and print it
+            Message[] messages = emailFolder.getMessages();
+            System.out.println("messages.length---" + messages.length);
+
+            for (int i = 0, n = messages.length; i < n; i++) {
+                Message message = messages[i];
+                System.out.println("---------------------------------");
+                System.out.println("Email Number " + (i + 1));
+                System.out.println("Subject: " + message.getSubject());
+                System.out.println("From: " + message.getFrom()[0]);
+                System.out.println("Text: " + message.getContent().toString());
+            }
+
+            // close the store and folder objects
+            emailFolder.close(false);
+            store.close();
+
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,22 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.util.Properties;
-import javax.mail.Folder;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.NoSuchProviderException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Store;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainDesign.fxml"));
         primaryStage.setTitle("Gmail Email Application - Dominic Heal");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
@@ -28,69 +21,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
 
-        String host = "pop.gmail.com";// change accordingly
-        String mailStoreType = "pop3";
-        String username = "abc@gmail.com";// change accordingly
-        String password = "*****";// change accordingly
-
-        check(host, mailStoreType, username, password);
-
     }
-
-        public static void check(String host, String storeType, final String user,
-                                 final String password)
-        {
-            try {
-
-                // create properties field
-                Properties properties = new Properties();
-
-                properties.put("mail.pop3s.host", host);
-                properties.put("mail.pop3s.port", "995");
-                properties.put("mail.pop3s.starttls.enable", "true");
-
-                // Setup authentication, get session
-                Session emailSession = Session.getInstance(properties,
-                        new javax.mail.Authenticator() {
-                            protected PasswordAuthentication getPasswordAuthentication() {
-                                return new PasswordAuthentication(
-                                        user, password);
-                            }
-                        });
-                // emailSession.setDebug(true);
-
-                // create the POP3 store object and connect with the pop server
-                Store store = emailSession.getStore("pop3s");
-
-                store.connect();
-
-                // create the folder object and open it
-                Folder emailFolder = store.getFolder("INBOX");
-                emailFolder.open(Folder.READ_ONLY);
-
-                // retrieve the messages from the folder in an array and print it
-                Message[] messages = emailFolder.getMessages();
-                System.out.println("messages.length---" + messages.length);
-
-                for (int i = 0, n = messages.length; i < n; i++) {
-                    Message message = messages[i];
-                    System.out.println("---------------------------------");
-                    System.out.println("Email Number " + (i + 1));
-                    System.out.println("Subject: " + message.getSubject());
-                    System.out.println("From: " + message.getFrom()[0]);
-                    System.out.println("Text: " + message.getContent().toString());
-                }
-
-                // close the store and folder objects
-                emailFolder.close(false);
-                store.close();
-
-            } catch (NoSuchProviderException e) {
-                e.printStackTrace();
-            } catch (MessagingException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+}
