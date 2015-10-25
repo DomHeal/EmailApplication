cea294d674ee24d6c2710519f3d61488aec4f224
DomHeal <Dominic.heal@europe.com>
2015-10-25 10:45:08 +0000

    Working Console Implementation

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,15 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javax.mail.*;
 import java.util.Properties;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Store;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Gmail Email Application - Dominic Heal");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
@@ -22,31 +28,69 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
 
-    Properties props = new Properties();
-    props.setProperty("mail.store.protocol", "imaps");
-    try {
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(userName, password);
-            }
-        });
-        Store store = session.getStore();
-        store.connect("imap.gmail.com", "email", "password");
-        Folder inbox = store.getFolder("INBOX");
-        inbox.open(Folder.READ_ONLY);
-        Message msg = inbox.getMessage(inbox.getMessageCount());
-        Address[] in = msg.getFrom();
-        for (Address address : in) {
-            System.out.println("FROM:" + address.toString());
-        }
-        Multipart mp = (Multipart) msg.getContent();
-        BodyPart bp = mp.getBodyPart(0);
-        System.out.println("SENT DATE:" + msg.getSentDate());
-        System.out.println("SUBJECT:" + msg.getSubject());
-        System.out.println("CONTENT:" + bp.getContent());
-    } catch (Exception mex) {
-        mex.printStackTrace();
+        String host = "pop.gmail.com";// change accordingly
+        String mailStoreType = "pop3";
+        String username = "abc@gmail.com";// change accordingly
+        String password = "*****";// change accordingly
+
+        check(host, mailStoreType, username, password);
+
     }
 
-}
-}
+        public static void check(String host, String storeType, final String user,
+                                 final String password)
+        {
+            try {
+
+                // create properties field
+                Properties properties = new Properties();
+
+                properties.put("mail.pop3s.host", host);
+                properties.put("mail.pop3s.port", "995");
+                properties.put("mail.pop3s.starttls.enable", "true");
+
+                // Setup authentication, get session
+                Session emailSession = Session.getInstance(properties,
+                        new javax.mail.Authenticator() {
+                            protected PasswordAuthentication getPasswordAuthentication() {
+                                return new PasswordAuthentication(
+                                        user, password);
+                            }
+                        });
+                // emailSession.setDebug(true);
+
+                // create the POP3 store object and connect with the pop server
+                Store store = emailSession.getStore("pop3s");
+
+                store.connect();
+
+                // create the folder object and open it
+                Folder emailFolder = store.getFolder("INBOX");
+                emailFolder.open(Folder.READ_ONLY);
+
+                // retrieve the messages from the folder in an array and print it
+                Message[] messages = emailFolder.getMessages();
+                System.out.println("messages.length---" + messages.length);
+
+                for (int i = 0, n = messages.length; i < n; i++) {
+                    Message message = messages[i];
+                    System.out.println("---------------------------------");
+                    System.out.println("Email Number " + (i + 1));
+                    System.out.println("Subject: " + message.getSubject());
+                    System.out.println("From: " + message.getFrom()[0]);
+                    System.out.println("Text: " + message.getContent().toString());
+                }
+
+                // close the store and folder objects
+                emailFolder.close(false);
+                store.close();
+
+            } catch (NoSuchProviderException e) {
+                e.printStackTrace();
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
