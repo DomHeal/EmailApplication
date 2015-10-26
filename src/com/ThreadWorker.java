70a58b55ac6decc9db7899f903ebb00d0b520ecb
DomHeal <Dominic.heal@europe.com>
2015-10-26 19:37:41 +0000

    ThreadWorker now checks for new emails

--- src/com/ThreadWorker.java
+++ src/com/ThreadWorker.java
@@ -1,4 +1,4 @@
-package sample;
+package com;
 
 import javax.mail.*;
 import java.util.Properties;
@@ -7,10 +7,12 @@ import java.util.Properties;
  * Created by Dominic on 25-Oct-15.
  */
 public class ThreadWorker implements Runnable {
-    public String host;
-    String storeType;
-    String user;
-    String password;
+
+    private static final long THIRTYSECONDS = 30000;
+    private String host;
+    private String storeType;
+    private String user;
+    private String password;
 
     public ThreadWorker(String host, String storeType, final String user,
                              final String password) {
@@ -23,6 +25,8 @@ public class ThreadWorker implements Runnable {
 
     @Override
     public void run() {
+        Folder emailFolder = null;
+        Store store = null;
         try {
 
             // create properties field
@@ -31,7 +35,6 @@ public class ThreadWorker implements Runnable {
             properties.put("mail.pop3s.host", host);
             properties.put("mail.pop3s.port", "995");
             properties.put("mail.pop3s.starttls.enable", "true");
-
 
             // Setup authentication, get session
             Session emailSession = Session.getInstance(properties,
@@ -44,30 +47,32 @@ public class ThreadWorker implements Runnable {
             // emailSession.setDebug(true);
 
             // create the POP3 store object and connect with the pop server
-            Store store = emailSession.getStore("pop3s");
+            store = emailSession.getStore("pop3s");
 
             store.connect();
 
             // create the folder object and open it
-            Folder emailFolder = store.getFolder("INBOX");
+            emailFolder = store.getFolder("INBOX");
             emailFolder.open(Folder.READ_ONLY);
 
             // retrieve the messages from the folder in an array and print it
-            Message[] messages = emailFolder.getMessages();
-            System.out.println("messages.length---" + messages.length);
+            //Message[] messages = emailFolder.getMessages();
 
-            for (int i = 0, n = messages.length; i < n; i++) {
-                Message message = messages[i];
-                System.out.println("---------------------------------");
-                System.out.println("Email Number " + (i + 1));
-                System.out.println("Subject: " + message.getSubject());
-                System.out.println("From: " + message.getFrom()[0]);
-                System.out.println("Text: " + message.getContent().toString());
+            while (true) {
+                System.out.println(emailFolder.getNewMessageCount());
+                Thread.sleep(THIRTYSECONDS);
             }
 
-            // close the store and folder objects
-            emailFolder.close(false);
-            store.close();
+//            System.out.println("messages.length---" + messages.length);
+//
+//            for (int i = 0, n = messages.length; i < n; i++) {
+//                Message message = messages[i];
+//                System.out.println("---------------------------------");
+//                System.out.println("Email Number " + (i + 1));
+//                System.out.println("Subject: " + message.getSubject());
+//                System.out.println("From: " + message.getFrom()[0]);
+//                System.out.println("Text: " + message.getContent().toString());
+//            }
 
         } catch (NoSuchProviderException e) {
             e.printStackTrace();
@@ -75,6 +80,14 @@ public class ThreadWorker implements Runnable {
             e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                emailFolder.close(false);
+                store.close();
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
