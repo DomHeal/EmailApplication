d1adb3fca5007ab6fade54bad0b110fe9fc243f3
DomHeal <Dominic.heal@europe.com>
2015-10-27 21:19:30 +0000

    Fixed Sound Paths

--- src/main/java/com/Soundboard.java
+++ src/main/java/com/Soundboard.java
@@ -1,12 +1,8 @@
 package com;
 
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import java.io.File;
 
 /**
  * Created by Dominic on 27-Oct-15.
@@ -17,7 +13,7 @@ public class Soundboard {
         try {
             AudioInputStream audioInputStream;
             audioInputStream = AudioSystem.getAudioInputStream(getClass().getClassLoader()
-                    .getResourceAsStream("sounds/blip.wav"));
+                    .getResourceAsStream("blip.wav"));
             final Clip clip = AudioSystem.getClip();
             clip.open(audioInputStream);
             clip.start();
